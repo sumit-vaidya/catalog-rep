7a96138d2e08a6def686a219f20242e60faf31d9
sumit-vaidya <vaidyasumit007@gmail.com>
2021-08-28 12:50:21 +0530

    first commit

---
+++ src/main/java/com/project/catalog/CatalogAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogAppApplication.class, args);
+	}
+
+}
